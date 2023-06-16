b9fe58046bbaaf520643cc6b42ad97fd4f7f7450
Quy Tran <100293867+Quytrw@users.noreply.github.com>
2023-06-16 16:21:58 +0700

    course1_kimquy

--- course_1/ViewController.swift
+++ course_1/ViewController.swift
@@ -9,11 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var RetryBtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        RetryBtn.layer.cornerRadius = 12
+        RetryBtn.layer.borderWidth = 1
+        RetryBtn.layer.borderColor = UIColor(named: "primary_color")?.cgColor
+        RetryBtn.titleLabel?.textColor = UIColor(named: "primary_color")
+        UINavigationBar.appearance().shadowImage = UIImage()
+        UINavigationBar.appearance().setBackgroundImage(UIImage(), for: .default)
     }
-
 
 }
 
