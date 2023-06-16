ebce4e91ea278a487f17be92b6e175edac7388bf
anhquoc2010 <muicamau007@gmail.com>
2023-06-16 11:38:48 +0700

    Initial Commit

---
+++ IOSTraning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOSTraning
+//
+//  Created by Mobile Dev 1 on 16/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
