4be5157d89140a192a4a38b654515e35e21433a0
Quy Tran <100293867+Quytrw@users.noreply.github.com>
2023-06-19 09:52:54 +0700

    Initial Commit

---
+++ course_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  course_2
+//
+//  Created by Mobile Dev on 19/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
