3d8fc09f24061425e0d066c4dc2471721130fa45
anhquoc2010 <muicamau007@gmail.com>
2023-06-20 21:44:25 +0700

    feat: course4 anhquoc

---
+++ Course4/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Course4
+//
+//  Created by Mobile Dev 1 on 20/06/2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
---
+++ Course4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Course4
+//
+//  Created by Mobile Dev 1 on 20/06/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
